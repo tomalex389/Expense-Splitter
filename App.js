54d01b90485189e02dfac0299f77a9548a2f4e1a
Tom P Alex <tomalex389@gmail.com>
2023-11-02 14:40:29 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+import SignUpScreen from './SignUpScreen';
+import ViewFriendList from './ViewFriendList';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUpScreen"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ViewFriendList"
+          component={ViewFriendList}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
